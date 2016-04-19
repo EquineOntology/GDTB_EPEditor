f4ca01f9efd9b079aeb9ef09bb8a4b062eb4e31f
Christian Fratta <christian.fratta@gmail.com>
2016-04-19 10:10:41 +0200

    Rename all textures to fit new schema

--- Assets/Gamedev Toolbelt/Editor/EditorPrefs Editor/Constants.cs
+++ Assets/Gamedev Toolbelt/Editor/EditorPrefs Editor/Constants.cs
@@ -2,24 +2,24 @@
 {
     public static class Constants
     {
-        public const string TEX_EDIT_DARK = "GUI/epeditor_EDIT_dark";
-        public const string TEX_ADD_DARK = "GUI/epeditor_ADD_dark";
-        public const string TEX_GET_DARK = "GUI/epeditor_GET_dark";
-        public const string TEX_REFRESH_DARK = "GUI/epeditor_REFRESH_dark";
-        public const string TEX_DELETE_DARK = "GUI/epeditor_DELETE_dark";
-        public const string TEX_DELETEALL_DARK = "GUI/epeditor_DELETEALL_dark";
-        public const string TEX_REMOVE_DARK = "GUI/epeditor_REMOVE_dark";
-        public const string TEX_SETTINGS_DARK = "GUI/epeditor_SETTINGS_dark";
+		public const string TEX_EDIT_DARK = "GUI/gdtb_EDIT_dark";
+		public const string TEX_ADD_DARK = "GUI/gdtb_ADD_dark";
+		public const string TEX_GET_DARK = "GUI/gdtb_GET_dark";
+		public const string TEX_REFRESH_DARK = "GUI/gdtb_REFRESH_dark";
+		public const string TEX_DELETE_DARK = "GUI/gdtb_DELETE_dark";
+		public const string TEX_DELETEALL_DARK = "GUI/gdtb_DELETEALL_dark";
+		public const string TEX_REMOVE_DARK = "GUI/gdtb_REMOVE_dark";
+		public const string TEX_SETTINGS_DARK = "GUI/gdtb_SETTINGS_dark";
 
-        public const string TEX_EDIT_LIGHT = "GUI/epeditor_EDIT_light";
-        public const string TEX_ADD_LIGHT = "GUI/epeditor_ADD_light";
-        public const string TEX_GET_LIGHT = "GUI/epeditor_GET_light";
-        public const string TEX_REFRESH_LIGHT = "GUI/epeditor_REFRESH_light";
-        public const string TEX_DELETE_LIGHT = "GUI/epeditor_DELETE_light";
-        public const string TEX_DELETEALL_LIGHT = "GUI/epeditor_DELETEALL_light";
-        public const string TEX_REMOVE_LIGHT = "GUI/epeditor_REMOVE_light";
-        public const string TEX_SETTINGS_LIGHT = "GUI/epeditor_SETTINGS_light";
-        public const string TEX_SCROLLBAR = "GUI/epeditor_scrollbar";
+		public const string TEX_EDIT_LIGHT = "GUI/gdtb_EDIT_light";
+		public const string TEX_ADD_LIGHT = "GUI/gdtb_ADD_light";
+		public const string TEX_GET_LIGHT = "GUI/gdtb_GET_light";
+		public const string TEX_REFRESH_LIGHT = "GUI/gdtb_REFRESH_light";
+		public const string TEX_DELETE_LIGHT = "GUI/gdtb_DELETE_light";
+		public const string TEX_DELETEALL_LIGHT = "GUI/gdtb_DELETEALL_light";
+		public const string TEX_REMOVE_LIGHT = "GUI/gdtb_REMOVE_light";
+		public const string TEX_SETTINGS_LIGHT = "GUI/gdtb_SETTINGS_light";
+		public const string TEX_SCROLLBAR = "GUI/gdtb_scrollbar";
 
         public const string FILE_GUISKIN = "GUI/epeditor_skin";
 
